df3e26e37d40635f3f07919dabf629732e618c0d
Gabriel Feitosa <cabeceirasmax@gmail.com>
2022-11-25 13:48:11 -0300

    Criando classe e consumindo com API JavaMail

---
+++ enviandoemail/src/main/java/br/com/enviandoemail/enviandoemail/App.java
@@ -0,0 +1,13 @@
+package br.com.enviandoemail.enviandoemail;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
